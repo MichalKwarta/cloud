462c8a9443800a3908238bee9fe5eb46c4ac26c4
MichalKwarta <Michu1164@gmail.com>
2022-05-11 20:55:15 +0200

    format files

--- Data_model.py
+++ Data_model.py
@@ -6,11 +6,13 @@ from collections import defaultdict
 class Data_model:
 
     def __init__(self,validators:list[Validator]|None = None):
+        
         self.instaces:list[Base_data_instance] = []
         self.validators = validators
 
 
     def add_instance(self,instance:Base_data_instance):
+
         if self.validators:
             validation_messages =  [validator.error_message for validator in self.validators if not validator.validate(instance)]
             instance['invalid_attribute_message'] = validation_messages
@@ -19,18 +21,21 @@ class Data_model:
         
         
     def add_many_instances(self,instances:list[Base_data_instance]):
+
         for instance in instances:
             self.add_instance(instance)
 
     def generate_validation_raport(self):
+
         grouped_invalid_instances = defaultdict(list)
         for instance in self.instaces:
             if not instance['is_valid'] and instance['invalid_attribute_message']:
                 for message in instance['invalid_attribute_message']:
                     grouped_invalid_instances[message].append(instance)
-
         return grouped_invalid_instances
+
     def print_validation_raport(self):
+
         if not self.instaces:
             print("No instances to generate report for")
             return 
--- Validator.py
+++ Validator.py
@@ -4,6 +4,7 @@ from data_instance import Cloud_data_instance
 
 class Validator:
     def __init__(self,field:str,validate_function:Callable[[object],bool],error_message:str):
+        
         self.field = field
         self.validate_function = validate_function
         self.error_message = error_message
@@ -11,7 +12,9 @@ class Validator:
     def validate(self,target:TypedDict)->bool:
         return self.validate_function(target[self.field])  
 class Cloud_validator(Validator):
+
     def __init__(self,field:str,validate_function:Callable[[object],bool],error_message:str):
+        
         super().__init__(field,validate_function,error_message)
         if field not in Cloud_data_instance.__annotations__.keys():
             raise ValueError(f"Field {field} is not a valid field for Cloud_data_instance")
